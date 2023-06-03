64145bf3cfc76fe7238acd9b0373d9c08007641b
oni <oni@oninoMacBook-Air.local>
2023-06-03 21:48:40 +0900

    fix

--- link.sh
+++ link.sh
@@ -2,3 +2,5 @@
 
 ln -snfv $HOME/dotfiles/nvim $HOME/.config/nvim
 ln -snfv $HOME/dotfiles/.zshrc ~/.zshrc
+ln -snfv $HOME/dotfiles/karabiner/1685779992.json ~/.config/karabiner/assets/complex_modifications/1685779992.json
+ln -snfv $HOME/dotfiles/karabiner/karabiner.json ~/.config/karabiner/karabiner.json
